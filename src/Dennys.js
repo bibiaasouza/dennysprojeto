bac53205b4bbb2f58856b52c4a59887408bb719e
BIANCA SOUZA DE JESUS <021.898692@senaicimatec.edu.br>
2024-04-19 13:20:24 -0300

    first commit

---
+++ src/Dennys.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.js'; 
+import './App.css';
+
+
+function Home() {
+  return (
+    <div>
+      <header className="capa">
+        <img src={require('./capa.jpg')} alt="capa home" width={950}/>
+      </header>
+    </div>
+    
+  
+         );
+} 
+  
+ 
+
+
+export default Home;
